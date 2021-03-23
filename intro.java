5ec975fb133f677ea00ef75b75295ff47aadae26
edumorris <eduuwachira@gmail.com>
2021-03-23 13:21:53 +0300

    First Java Program

---
+++ intro.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        /*
+            // This is a nested single line comment. Should work.
+            This should not be seen by the compiler
+        */
+    }
+}
